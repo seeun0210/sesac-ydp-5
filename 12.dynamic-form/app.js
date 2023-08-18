5ae18bdffa104b4270ed7c7fbb6e9fc6c6da1ef9
KimSeEun <cocobell3@naver.com>
2023-08-18 14:43:05 +0900

    study: 동적 폼 전송

---
+++ 12.dynamic-form/app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const PORT = 8000;
+
+app.set('view engine', 'ejs');
+app.use('/views', express.static(__dirname + '/views'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.render('dynamic');
+});
+
+app.get('/ajax', (req, res) => {
+  console.log(req.query);
+  res.send(req.query);
+});
+app.post('/ajax', (req, res) => {
+  console.log(req.body);
+  res.send(req.body);
+});
+app.listen(PORT, function () {
+  console.log(`Port ${PORT} is opening!`);
+});
