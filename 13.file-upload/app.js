a9029f1b2da252aeff4ad854a4a07f32378e7bd9
KimSeEun <cocobell3@naver.com>
2023-08-21 15:32:06 +0900

    study: multer 세부설정

--- 13.file-upload/app.js
+++ 13.file-upload/app.js
@@ -4,10 +4,30 @@ const PORT = 8000;
 
 //multer관련 설정
 const multer = require('multer');
+const path = require('path'); //경로에 관한 내장모듈
 const upload = multer({
   dest: 'uploads/', //dest: 클라이언트가 업로드한 파일을 저장할 서버측 경로
 }); //파일이 추가되면 uploads랴는 폴더가 생성되고 그 안에 업로드한 파일이 추가됨
-
+//multer 세부 설정
+const uploadDetail = multer({
+  //storage:저장할 공간에 대한 정보
+  storage: multer.diskStorage({
+    destination(req, file, done) {
+      //done: callback함수
+      done(null, 'uploads/'); //파일을 업로드 할 경로를 설정
+    },
+    filename(req, file, done) {
+      const ext = path.extname(file.originalname); //extname():파일"확장자"를 추출
+      done(null, path.basename(file.originalname, ext) + Date.now() + ext); //basename():원본 파일에서 확장자를 제외한 파일 이름만 추출
+      //파일이름+날짜+확장자
+      //날짜를 추가해서(Date.now()) 파일이름을 저장하는 이유는??
+      //1. 파일 이름 중복을 막기 위해서
+      //2. 파일 이름만 보고 파일이 저장된 시점 유추 가능
+    },
+  }),
+  //limits:파일 제한 정보
+  limits: { fileSize: 5 * 1024 * 1024 }, //5MB로 파일 크기를 제한,
+});
 app.set('view engine', 'ejs');
 app.use('/views', express.static(__dirname + '/views'));
 app.use(express.urlencoded({ extended: true }));
@@ -19,7 +39,7 @@ app.get('/', (req, res) => {
 //1. single():하나의 파일을 업로드
 //upload.single('userfile'): 클라이언트의 요청이 들어오면 multer설정(upload변수)에 따라 파일을 업로드 한 후, req.file 객체를 생성
 //single() 인자는 input 태그의 name 속성과 일치시켜야함
-app.post('/upload', upload.single('userfile'), (req, res) => {
+app.post('/upload', uploadDetail.single('userfile'), (req, res) => {
   res.send('파일 업로드 완료!');
   //req.file:파일 업로드 정보
   //req.body: 파일 외의 정보들
